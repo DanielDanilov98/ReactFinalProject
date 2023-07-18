a994a7c36d0123de42c728ede2fcfe8b9ab14291
Daniel Danilov <danieldanilov1998@gmail.com>
2023-07-18 17:15:24 +0300

    last

--- src/users/pages/EditUserPage.jsx
+++ src/users/pages/EditUserPage.jsx
@@ -1,3 +1,14 @@
-const EditUserPage = () => {};
+import { Grid } from "@mui/material";
+
+const EditUserPage = () => {
+  return (
+    <>
+      <Grid>
+        <h1>EditUserPage</h1>
+      </Grid>
+      <Grid>dont forget build this</Grid>
+    </>
+  );
+};
 
 export default EditUserPage;
--- src/users/pages/UserProfile.jsx
+++ src/users/pages/UserProfile.jsx
@@ -1,3 +1,14 @@
-const UserProfile = () => {};
+import { Grid } from "@mui/material";
+
+const UserProfile = () => {
+  return (
+    <>
+      <Grid>
+        <h1>UserProfile</h1>
+      </Grid>
+      <Grid>dont forget build this</Grid>
+    </>
+  );
+};
 
 export default UserProfile;
